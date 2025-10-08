00033c2e0fe0a057380a8df2a0a35581a621133d
cristiantommasi <tommasi@gmail.com>
2025-10-08 19:13:21 -0300

    se agrega ruta para buscar libros

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -5,3 +5,7 @@ const librosController = require('../controllers/book.controller.js');
 // Rutas
 router.get('/', librosController.obtenerCatalogo);
 router.get('/buscar', librosController.buscar);
+router.get('/:id', librosController.verDetalle);
+
+
+module.exports = router;
