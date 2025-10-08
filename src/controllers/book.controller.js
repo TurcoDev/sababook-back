a097cb7ccde2f9b40126cc722d50b75754a45b6f
cristiantommasi <tommasi@gmail.com>
2025-10-08 19:04:45 -0300

    se crea controlador para mostrar catalogo de libros

---
+++ src/controllers/book.controller.js
@@ -0,0 +1,12 @@
+const LibroModel = require('../models/librosModel');
+
+// 1. Mostrar catálogo
+exports.obtenerCatalogo = async (req, res) => {
+  try {
+    const libros = await LibroModel.obtenerTodos();
+    res.json(libros);
+  } catch (error) {
+    console.error('Error al obtener catálogo:', error);
+    res.status(500).json({ mensaje: 'Error al obtener libros' });
+  }
+};
