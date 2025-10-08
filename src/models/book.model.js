5159c30f77c27db66ff94caa33bef895f4f263de
cristiantommasi <tommasi@gmail.com>
2025-10-08 19:09:31 -0300

    se agrega modelo para ver detalle de un libro

--- src/models/book.model.js
+++ src/models/book.model.js
@@ -4,3 +4,8 @@ exports.obtenerTodos = async () => {
   const result = await pool.query('SELECT * FROM libro');
   return result.rows;
 };
+
+exports.obtenerPorId = async (id) => {
+  const result = await pool.query('SELECT * FROM libro WHERE libro_id = $1', [id]);
+  return result.rows[0];
+};
