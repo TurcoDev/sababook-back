631434f51d13d2fca10741a63816fe3651d75380
cristiantommasi <tommasi@gmail.com>
2025-10-08 19:21:40 -0300

    se agrega ruta para borrado de un libro

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -7,6 +7,8 @@ router.get('/', librosController.obtenerCatalogo);
 router.get('/buscar', librosController.buscar);
 router.get('/:id', librosController.verDetalle);
 router.put('/:id', librosController.actualizar); 
+router.delete('/:id', librosController.eliminar);                 // Borrado físico
+
 
 
 module.exports = router;
