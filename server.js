762b92ad0d052ee1a9ba03d643cbd332ad0c73b3
Lucas Salvatori <lsalvatori.hms@gmail.com>
2025-10-15 21:25:42 -0300

    se agrega ruta de front a cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'null'],
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000', 'null'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
